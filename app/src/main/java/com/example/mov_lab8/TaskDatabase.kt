e7a4bc2f7f6d83bae7490aca152dcc4a045a353d
iTovarr <13m.tovar.m@gmail.com>
2026-05-11 17:21:19 -0500

    feat: configurar TaskDatabase y proveedor de instancia

--- app/src/main/java/com/example/mov_lab8/TaskDatabase.kt
+++ app/src/main/java/com/example/mov_lab8/TaskDatabase.kt
@@ -1,2 +1,10 @@
 package com.example.mov_lab8
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+
+@Database(entities = [Task::class], version = 1)
+abstract class TaskDatabase : RoomDatabase() {
+    abstract fun taskDao(): TaskDao
+}
